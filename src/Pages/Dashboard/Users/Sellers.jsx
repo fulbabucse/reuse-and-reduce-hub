35cd1289e88a41e2b175f961411da7a14cefe44d
Fulbabu <fulbabucse@gmail.com>
2022-11-27 00:36:09 +0600

    admin verified seller complete

--- src/Pages/Dashboard/Users/Sellers.jsx
+++ src/Pages/Dashboard/Users/Sellers.jsx
@@ -37,8 +37,8 @@ const Sellers = () => {
       });
   };
 
-  const handleSellerVerify = (id) => {
-    fetch(`http://localhost:5000/verify-seller?id=${id}`, {
+  const handleSellerVerify = (email) => {
+    fetch(`http://localhost:5000/verify-seller?email=${email}`, {
       method: "PATCH",
       headers: {
         authorization: `Bearer ${localStorage.getItem("reuseReduceToken")}`,
@@ -146,7 +146,7 @@ const Sellers = () => {
                         ) : (
                           <>
                             <button
-                              onClick={() => handleSellerVerify(user?._id)}
+                              onClick={() => handleSellerVerify(user?.email)}
                               className="inline-block px-2 py-2 bg-baseColor text-white font-medium text-sm leading-tight rounded-md shadow-md  hover:shadow-2xl focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition-colors duration-200 ease-in-out"
                             >
                               Verify Seller
