907ebddf08f385bf22ac8bcaadb974245dca08dd
Fulbabu <fulbabucse@gmail.com>
2022-11-27 21:21:08 +0600

    product card modify

--- src/Pages/Shared/ProductCard/ProductCard.jsx
+++ src/Pages/Shared/ProductCard/ProductCard.jsx
@@ -96,6 +96,7 @@ const ProductCard = ({ product, handleBookingProduct }) => {
               </button>
               <button
                 onClick={() => handleReportProduct(_id)}
+                disabled={adminRoleCheck?.role === "admin"}
                 className="inline-block px-2 py-2 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-500 hover:shadow-lg focus:bg-red-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-500 active:shadow-lg transition duration-150 ease-in-out"
               >
                 Report to Admin
